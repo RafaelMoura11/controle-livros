4ce137d2ffdb1f9558427065e002491bf42f5c48
Rafael Moura <87615082+RafaelMoura11@users.noreply.github.com>
2025-12-24 13:26:07 -0300

    Add routes for book management with Livewire components

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
+use App\Livewire\Books\Index as BooksIndex;
+use App\Livewire\Books\Form as BooksForm;
+use App\Livewire\Books\Show as BooksShow;
 use Illuminate\Support\Facades\Route;
+
+Route::redirect('/', '/books');
+
+Route::get('/books', BooksIndex::class)->name('books.index');
+Route::get('/books/create', BooksForm::class)->name('books.create');
+Route::get('/books/{book}/edit', BooksForm::class)->name('books.edit');
+Route::get('/books/{book}', BooksShow::class)->name('books.show');
 
 Route::get('/', function () {
     return view('welcome');
